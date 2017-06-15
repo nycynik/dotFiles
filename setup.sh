6f54bb1d7a156f14230db0b19fcf228d76aabb36
michael lynch <nycynik@gmail.com>
2017-06-15 09:15:43 -0400

    Added autocomplete to setup

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ git config --global alias.type 'cat-file -t'
 git config --global alias.dump 'cat-file -p'
 git config --global alias.hist 'log --pretty=format:\"%h %ad | %s%d [%an]\" --graph --date=short'
 git config --global core.excludesfile ~/.gitignore_global
+git config --global help.autocorrect 5
 
 echo bin setup
 if [ ! -d "$HOME/bin/" ]; then
