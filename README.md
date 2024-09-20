d5229abe9cbe3d53bf954c1f1a1dd9ad527dff5a
Mike Lynch <nycynik@gmail.com>
2024-09-20 14:58:53 -0700

    updated and moved to zsh

--- README.md
+++ README.md
@@ -13,12 +13,12 @@ Python | Java | Node | Ruby
 Step1: Check out the repo in your home dir, that will make a hidden directory called .dotfiles in home.
 
      cd ~
-     git clone --depth=1 git@github.com:nycynik/dotFiles.git
+     git clone --depth=1 git@github.com:nycynik/dotFiles.git .dotfiles
      rm -rf ./.dotfiles/.git
      
 Step2: Run the setup file and follow the prompts
 
-     . ./setup.sh
+     . .dotfiles/setup.sh
      
 
 # Future Ideas
--- setup.sh
+++ setup.sh
@@ -1,36 +1,28 @@
-#!/bin/sh
-
-if [[ ! -d "${PWD}/.dotfiles" ]] ; then
-	echo "ABORTING: No .dotfiles found in current directory."
-	echo "          Script must be run from root of dotfiles repo."
-    	[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don'$
-fi
+#!/bin/bash
 
 if [[ "${PWD}" != "${HOME}" ]] ; then
-        echo "NOTE: script is not running from your home dir" 
+        echo "NOTE: script is not running from your home dir. (HOME set to $HOME)" 
         read -p "Create symlink in ${PWD} [Y/n]? " -n 1 -r
         if [[ "$REPLY" =~ ^[yY]$ ]] ; then
-                ln -s ./dotfiles ${PWD}/.dotfiles
+                ln -s ./dotfiles ${PWD}
         fi
 fi
 
-echo bin setup
-if [[ ! -d "$HOME/bin/" ]]; then
-	mkdir "$HOME/bin"
-fi
+echo ==========================================================
+echo Global Set up
+echo ==========================================================
 
-echo git setup
-if [[ ! -d "$HOME/.gitignore_global" ]]; then
-	touch "$HOME/.gitignore_global"
-	cat ./.dotfiles/gitignore_global >> "$HOME/.gitignore_global"
-fi
+echo Hi! 🎉
+echo what is your name?
+read USERNAME
 
-if [[ ! -d "/usr/local/git/contrib/completion/" ]]; then
-	brew install git bash-completion
-fi
+echo what is your email?
+read USEREMAIL
 
+echo ----------------------------------------------------------
 echo First Time Setup
-read -p "Run first time setup? " -n 1 -r
+echo ----------------------------------------------------------
+read -p "Hey $username, 📛\nAre you ready to run first time setup? " -n 1 -r
 echo .   
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
@@ -38,16 +30,65 @@ then
 fi
 
 echo ==========================================================
-echo Running first time setup
+echo Running first time setup  🚀🚀🚀
 echo ==========================================================
 
-if [[ ! -d "$HOME/.bash_profile" ]]; then
-	touch "$HOME/.bash_profile"
-	cat ./.dotfiles/bash_profile >> "$HOME/.bash_profile"
+echo bin setup
+echo ----------------------------------------------------------
+if [[ ! -d "$HOME/bin/" ]]; then
+	mkdir "$HOME/bin"
 fi
 
-echo "bash setup"
-echo git config
+echo Homebrew setup 
+echo ----------------------------------------------------------
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	echo Run brew doctor when finished.
+fi
+brew update
+brew upgrade
+
+echo  Setting up zsh
+echo ----------------------------------------------------------
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+if [[ ! -d "$HOME/.p10k.zsh" ]]; then
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+	pk10k configure
+fi
+
+if [[ ! -d "${HOME}/.zshrc" ]] ; then
+	cp ./.zshrc "${HOME}/.zshrc"
+else
+cat <<EOT >> "${HOME}/.zshrc"
+if [[ -f ~/.dotfiles/aliases ]]; then
+    source ~/.dotfiles/aliases
+fi
+
+# personal functions
+if [[ -f ~/.dotfiles/functions ]]; then
+    source ~/.dotfiles/functions
+fi
+EOT
+fi
+
+echo git setup
+echo ----------------------------------------------------------
+echo NOTE: add your ssh keys
+if [ -x "$(command -v git)" ]; then
+	brew install git
+fi
+
+if [[ ! -d "$HOME/.gitconfig" ]]; then
+	cp ./.gitconfig "$HOME/.gitconfig"
+fi
+
+if [[ ! -d "$HOME/.gitignore_global" ]]; then
+	touch "$HOME/.gitignore_global"
+	cat ./gitignore_global >> "$HOME/.gitignore_global"
+fi
 git config --global core.editor nano
 git config --global alias.co checkout
 git config --global alias.br branch
@@ -62,29 +103,40 @@ git config --global alias.hist 'log --pretty=format:"%h %ad | %s%C(auto)%d$Crese
 git config --global core.excludesfile ~/.gitignore_global
 git config --global help.autocorrect 5
 
-# setup quicklook
-brew update
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv qlimagesize webpquicklook suspicious-package
+echo Dev tools and CLI tools
+echo ----------------------------------------------------------
+
 brew install betterzip
-brew install jq bash
 
 # dev tools
 brew install watchman
 brew install httpie
 brew install tree jq
 
+if [[ ! -d "$HOME/.nvm" ]]; then
+	mkdir ~/.nvm
+	brew install nvm
+	cat <<EOT >> "${HOME}/.zshrc"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && \. "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" # This loads nvm
+EOT
+	nvm install --lts
+fi
+
+# java
+brew install maven
+
 # window tools
 brew install rectangle
 
-# zsh
-brew install zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# xcode
+if [ -x "$(command -v xcode-select)" ]; then
+	xcode-select --install
+fi
 
 echo "Finished Setup"
-echo .
-echo "You may want to remove the lic and ReadMe files from your home directory now."
 
-read -p "Delete LICENSE and README.md now? " -n 1 -r
+read -p "Clean up? " -n 1 -r
 echo .
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
